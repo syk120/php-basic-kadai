f3d3c62fab26d40b3a58fdcd58280e3149e5e112
syk120 <symsayaka0120@gmail.com>
2025-11-11 15:05:24 +0900

    kadai_007追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>PHP課題編</title>
+</head>
+
+<body>
+   <p>
+       <?php
+       // 連想配列に値を代入する
+       $Array = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+       // 連想配列の値を出力する
+       print_r($Array);
+        ?>
+   </p>
+</body>
+
+</html>
